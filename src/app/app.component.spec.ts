ffafaa82652e4a4d1b785dc142ec60ca67010395
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2020-09-10 12:34:33 +0200

    chore(deploy): npm-script deploy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +15,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'km-calculator'`, () => {
+  it(`should have as title 'calculadora-fct'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('km-calculator');
+    expect(app.title).toEqual('calculadora-fct');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to km-calculator!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to calculadora-fct!',
+    );
   });
 });
